5d94af4975dbbda9e9ba3233eee10706df70f496
Krishna Sharma <krishnasharma3011@gmail.com>
2026-01-01 22:50:16 +0530

    Create rag.py

---
+++ core/rag.py
@@ -0,0 +1,26 @@
+from langchain.chains import RetrievalQA
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.llms import HuggingFacePipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+
+def load_rag():
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    db = FAISS.load_local(
+        "faiss_index",
+        embeddings,
+        allow_dangerous_deserialization=True
+    )
+
+    model_name = "google/flan-t5-base"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+    pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
+
+    llm = HuggingFacePipeline(pipeline=pipe)
+
+    return db, llm
